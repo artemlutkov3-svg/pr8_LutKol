61547d8d7b8c50d96fd7f1692f3d76d50d3ed74c
Лутковский Артем <artemlutkov3@gmail.com>
2026-04-09 02:05:30 +0300

    dev: добавлены цифры и спецсимволы

--- password.py
+++ password.py
@@ -2,7 +2,7 @@ import random
 import string
 
 def generate_password(n):
-    symbols = string.ascii_lowercase + string.ascii_uppercase
+    symbols = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
     password = ''.join(random.choice(symbols) for _ in range(n))
     return password
 
