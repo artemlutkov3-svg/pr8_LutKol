a421b9c4b09aa785ea75098e774274f60c56e322
Лутковский Артем <artemlutkov3@gmail.com>
2026-04-09 01:59:02 +0300

    добавлена функция генерации пароля из N символов

---
+++ password.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def generate_password(n):
+    symbols = string.ascii_lowercase
+    password = ''.join(random.choice(symbols) for _ in range(n))
+    return password
+
+n = int(input("Введите длину пароля: "))
+print("Сгенерированный пароль:", generate_password(n))
